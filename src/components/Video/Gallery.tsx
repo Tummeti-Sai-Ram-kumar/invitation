178a7b6ea05e2892a1488b696af836c43f661319
Tummeti Sai Ram Kumar <tummetisairamkumar1234@gmail.com>
2026-04-27 14:50:27 +0530

    Added Video Poster

--- src/components/Video/Gallery.tsx
+++ src/components/Video/Gallery.tsx
@@ -4,6 +4,8 @@ import videoSource1 from "../../assets/video1.mp4";
 import videoSource2 from "../../assets/video2.mp4";
 import imageSource1 from "../../assets/image1.jpeg";
 import imageSource2 from "../../assets/image2.jpeg";
+import imageSource3 from "../../assets/image3.jpeg";
+import imageSource4 from "../../assets/image4.jpeg";
 
 import styles from "./Video.module.css";
 
@@ -11,10 +13,10 @@ const Gallery: FC = (): ReactElement => (
     <div className={styles.video__container}>
         <img src={imageSource1} className={styles.video__image} />
         <img src={imageSource2} className={styles.video__image} />
-        <video controls className={styles.video__player}>
+        <video controls className={styles.video__player} poster={imageSource3}>
           <source src={videoSource1} type="video/mp4" />
         </video>
-        <video controls className={styles.video__player}>
+        <video controls className={styles.video__player} poster={imageSource4}>
           <source src={videoSource2} type="video/mp4" />
         </video>
       </div>    
